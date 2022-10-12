0e0b1f5145097dc32906090b714e007501185a5c
MayconTrix <maycon.tiutrix@gmail.com>
2022-10-12 19:45:25 -0300

    first commit

---
+++ src/components/Button.stories.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Meta, StoryObj } from "@storybook/react";
+import { Button, ButtonProps } from "./Button";
+
+export default {
+  title: "Components/Button",
+  component: Button,
+  args: {
+    children: "Create account",
+  },
+  argTypes: {
+    size: {
+      size: {
+        options: ["sm", "md", "lg"],
+        control: {
+          type: "inline-radio",
+        },
+      },
+    },
+  },
+} as Meta<ButtonProps>;
+
+export const Default: StoryObj<ButtonProps> = {};
