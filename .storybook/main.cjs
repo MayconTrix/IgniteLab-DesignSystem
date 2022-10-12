d2e5a51cbc3fee98452532239c2a324f5030dd45
MayconTrix <maycon.tiutrix@gmail.com>
2022-10-12 20:02:51 -0300

    ci: Add publish storybook workflow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -12,4 +12,11 @@ module.exports = {
   features: {
     storyStoreV7: true,
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === "PRODUCTION") {
+      config.base = "/IgniteLab-DesignSystem/";
+    }
+
+    return config;
+  },
 };
