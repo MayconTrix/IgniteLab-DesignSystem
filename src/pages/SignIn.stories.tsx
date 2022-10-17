9356a6647b4802f2e22200a10eb777d4439f9189
MayconTrix <maycon.tiutrix@gmail.com>
2022-10-17 10:39:01 -0300

    Add pages

---
+++ src/pages/SignIn.stories.tsx
@@ -0,0 +1,25 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { within, userEvent } from "@storybook/testing-library";
+import { expect } from "@storybook/jest";
+import { SignIn } from "./SignIn";
+
+export default {
+  title: "Pages/Sign in",
+  component: SignIn,
+  args: {},
+  argTypes: {},
+} as Meta;
+
+export const Default: StoryObj = {
+  play: ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    userEvent.type(
+      canvas.getByPlaceholderText("Digite seu e-mail"),
+      "tiutrix@hotmail.com"
+    );
+    userEvent.type(canvas.getByPlaceholderText("********"), "12345678");
+
+    userEvent.click(canvas.getByRole("button"));
+  },
+};
