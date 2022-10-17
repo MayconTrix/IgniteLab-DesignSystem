79fd96080f101510e9b89ce0054879a5631079f2
MayconTrix <maycon.tiutrix@gmail.com>
2022-10-17 11:12:59 -0300

    Add SignIn/MockServiceWorker

--- src/pages/SignIn.stories.tsx
+++ src/pages/SignIn.stories.tsx
@@ -1,6 +1,7 @@
 import { Meta, StoryObj } from "@storybook/react";
-import { within, userEvent } from "@storybook/testing-library";
+import { within, userEvent, waitFor } from "@storybook/testing-library";
 import { expect } from "@storybook/jest";
+import { rest } from "msw";
 import { SignIn } from "./SignIn";
 
 export default {
@@ -8,10 +9,23 @@ export default {
   component: SignIn,
   args: {},
   argTypes: {},
+  parameters: {
+    msw: {
+      handlers: [
+        rest.post("/sessions", (req, res, ctx) => {
+          return res(
+            ctx.json({
+              message: "Login realizado!",
+            })
+          );
+        }),
+      ],
+    },
+  },
 } as Meta;
 
 export const Default: StoryObj = {
-  play: ({ canvasElement }) => {
+  play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
 
     userEvent.type(
@@ -21,5 +35,9 @@ export const Default: StoryObj = {
     userEvent.type(canvas.getByPlaceholderText("********"), "12345678");
 
     userEvent.click(canvas.getByRole("button"));
+
+    await waitFor(() => {
+      return expect(canvas.getByText("Login Realizado!")).toBeInTheDocument();
+    });
   },
 };
--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,5 +1,6 @@
 import { Checkbox } from "@radix-ui/react-checkbox";
 import { FormEvent, useState } from "react";
+import axios from "axios";
 import { Envelope, Lock } from "phosphor-react";
 import { Button } from "../components/Button";
 import { Heading } from "../components/Heading";
@@ -10,8 +11,13 @@ import { Logo } from "../Logo";
 export function SignIn() {
   const [isUserSignedIn, setIsUserSignedIn] = useState(false);
 
-  function handleSignIn(event: FormEvent) {
+  async function handleSignIn(event: FormEvent) {
     event.preventDefault();
+
+    await axios.post("/sessions", {
+      email: "tiutrix@hotmail.com",
+      password: "12345678",
+    });
 
     setIsUserSignedIn(true);
   }
